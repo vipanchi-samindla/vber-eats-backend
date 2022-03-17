49829831a8f0fa653f4b7c3961694ff359299a71
Yogeshp0012 <yogeshp0012@gmail.com>
2022-03-17 08:47:35 +0530

    ESLint modifications

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
 const app: Application = express();
@@ -8,5 +9,6 @@ app.get('/', (_req, res) => {
   res.send('Server Works');
 });
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App is listening on port ${port}`);
 });
